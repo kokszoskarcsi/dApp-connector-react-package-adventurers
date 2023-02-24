6b147ebe7cd20316dd09acfe0bd31987311c175e
kokszoskarcsi <karolyadventurers@gmail.com>
2023-02-24 19:42:09 +0100

    api key moved as prop

--- src/components/Requirements/Requirements.js
+++ src/components/Requirements/Requirements.js
@@ -11,25 +11,18 @@ import "../../styles.css";
 import NautilusLogo from "../../assets/NautilusLogo.png";
 
 // Initialize Firebase
-const app = initializeApp({
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-});
-const dbRef = ref(getDatabase(app));
-const auth = getAuth(app);
+
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
 const GET_USER_NFT_INTERVAL_DELAY = 10000;
 
-export const ErgoDappConnector = ({ color, setUserNFTs }) => {
+export const ErgoDappConnector = ({ color, setUserNFTs, databaseKey }) => {
+  const app = initializeApp(databaseKey);
+  const dbRef = ref(getDatabase(app));
+  const auth = getAuth(app);
+
   const [user, setUser] = useState(null);
   const [open, setOpen] = useState(true);
   const [ergoWallet, setErgoWallet] = useState();
--- src/stories/Requirements.stories.js
+++ src/stories/Requirements.stories.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { storiesOf } from "@storybook/react";
 import { ErgoDappConnector } from "../components/Requirements";
 
@@ -6,5 +6,13 @@ const stories = storiesOf("App test", module);
 
 stories.add("App test", () => {
   const [userNFTs, setUserNFTs] = useState([]);
-  return <ErgoDappConnector color={"orange"} setUserNFTs={setUserNFTs} />;
+  //API KEY REQUIRED
+
+  return (
+    <ErgoDappConnector
+      color={"orange"}
+      setUserNFTs={setUserNFTs}
+      databaseKey={APIKey}
+    />
+  );
 });
